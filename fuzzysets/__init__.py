da071901d2687ba58747c9310773a60b67ef3c1d
Stiliyan Dragnev <stiliyan.dragnev@gmail.com>
2021-02-15 20:33:24 +0200

    Make names available from fuzzysets

--- fuzzysets/__init__.py
+++ fuzzysets/__init__.py
@@ -1,1 +1,20 @@
+from fuzzysets.plot import (
+    plot_fuzzy_set,
+    plot_fuzzy_set_operations,
+    plot_tfn_operations,
+)
+from fuzzysets.sets.base import (
+    alpha_cut,
+    complement,
+    s_norm,
+    t_norm,
+)
+from fuzzysets.sets.continuous import (
+    ContinuousDomain,
+    ContinuousFuzzySet,
+)
+from fuzzysets.sets.finite import (
+    FiniteDomain,
+    FiniteFuzzySet,
+)
 from fuzzysets.tfn import TriangularFuzzyNumber as TFN
