7cc97829e0522734b4db0bb7279969c339fc35f7
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2024-11-09 09:31:54 +0000

    Add more tests for continuous fuzzy sets

--- tests/test_continuous_fs.py
+++ tests/test_continuous_fs.py
@@ -144,3 +144,222 @@ class TestContinuousFuzzySet:
         fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.4), lambda x: 1.0)
 
         assert not fs1 == fs2
+
+    def test_ne_if_other_is_not_a_continuous_fuzzy_set_returns_true(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        assert fs != "not a continuous fuzzy set"
+
+    def test_ne_if_domains_are_not_equal_returns_true(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        assert fs1 != fs2
+
+    def test_ne_if_domains_are_equal_does_pointwise_comparison_with_smaller_step(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.6), lambda x: 0.2)
+
+        assert not fs1 != fs2
+
+    def test_ne_if_membership_degrees_are_not_equal_returns_true(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.4), lambda x: 1.0)
+
+        assert fs1 != fs2
+
+    def test_lt_when_other_is_not_a_continuous_fuzzy_set_raises_type_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(TypeError):
+            fs < "not a continuous fuzzy set"
+
+    def test_lt_if_domains_are_not_equal_returns_false(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        assert not fs1 < fs2
+
+    def test_lt_if_domains_are_equal_does_pointwise_comparison_with_smaller_step(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.6), lambda x: 0.2)
+
+        assert not fs1 < fs2
+
+    def test_lt_when_lhs_is_a_proper_subset_of_rhs_returns_true(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.1)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.4), lambda x: 0.2)
+
+        assert lhs < rhs
+
+    def test_gt_when_other_is_not_a_continuous_fuzzy_set_raises_type_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(TypeError):
+            fs > "not a continuous fuzzy set"
+
+    def test_gt_if_domains_are_not_equal_returns_false(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        assert not fs1 > fs2
+
+    def test_gt_if_domains_are_equal_does_pointwise_comparison_with_smaller_step(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.6), lambda x: 0.2)
+
+        assert not fs1 > fs2
+
+    def test_gt_when_lhs_is_a_proper_superset_of_rhs_returns_true(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.4), lambda x: 0.1)
+
+        assert lhs > rhs
+
+    def test_le_when_other_is_not_a_continuous_fuzzy_set_raises_type_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(TypeError):
+            fs <= "not a continuous fuzzy set"
+
+    def test_le_if_domains_are_not_equal_returns_false(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        assert not fs1 <= fs2
+
+    def test_le_if_domains_are_equal_does_pointwise_comparison_with_smaller_step(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.6), lambda x: 0.2)
+
+        assert lhs <= rhs
+
+    def test_le_when_lhs_is_a_subset_of_rhs_returns_true(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.1)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.4), lambda x: 0.2)
+
+        assert lhs <= rhs
+
+    def test_ge_when_other_is_not_a_continuous_fuzzy_set_raises_type_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(TypeError):
+            fs >= "not a continuous fuzzy set"
+
+    def test_ge_if_domains_are_not_equal_returns_false(self):
+        fs1 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        fs2 = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        assert not fs1 >= fs2
+
+    def test_ge_if_domains_are_equal_does_pointwise_comparison_with_smaller_step(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.6), lambda x: 0.2)
+
+        assert lhs >= rhs
+
+    def test_ge_when_lhs_is_a_superset_of_rhs_returns_true(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.5), lambda x: 0.2)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 3.0, 0.4), lambda x: 0.1)
+
+        assert lhs >= rhs
+
+    def test_t_norm_if_other_is_not_a_continuous_fuzzy_set_raises_type_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(TypeError):
+            fs.t_norm("not a continuous fuzzy set")
+
+    def test_t_norm_if_norm_doesnt_return_membership_degrees_raises_value_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(ValueError):
+            fs.t_norm(fs, lambda x, y: "not a membership degree")
+    
+    def test_t_norm_when_domains_are_different_raises_value_error(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        with pytest.raises(ValueError):
+            lhs.t_norm(rhs)
+
+    def test_t_norm_uses_min_by_default(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        lhs = ContinuousFuzzySet(domain, lambda x: 0.1)
+        rhs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert lhs.t_norm(rhs) == ContinuousFuzzySet(domain, lambda x: 0.1)
+
+    def test_t_norm_with_custom_norm(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        lhs = ContinuousFuzzySet(domain, lambda x: 0.5)
+        rhs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert lhs.t_norm(rhs, lambda x, y: x * y) == ContinuousFuzzySet(domain, lambda x: 0.2)
+
+    def test_s_norm_if_other_is_not_a_continuous_fuzzy_set_raises_type_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(TypeError):
+            fs.s_norm("not a continuous fuzzy set")
+
+    def test_s_norm_if_norm_doesnt_return_membership_degrees_raises_value_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(ValueError):
+            fs.s_norm(fs, lambda x, y: "not a membership degree")
+
+    def test_s_norm_when_domains_are_different_raises_value_error(self):
+        lhs = ContinuousFuzzySet(ContinuousDomain(1.0, 2.6, 0.5), lambda x: 0.0)
+        rhs = ContinuousFuzzySet(ContinuousDomain(1.0, 2.7, 0.5), lambda x: 0.0)
+
+        with pytest.raises(ValueError):
+            lhs.s_norm(rhs)
+
+    def test_s_norm_uses_max_by_default(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        lhs = ContinuousFuzzySet(domain, lambda x: 0.2)
+        rhs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert lhs.s_norm(rhs) == ContinuousFuzzySet(domain, lambda x: 0.4)
+
+    def test_s_norm_with_custom_norm(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        lhs = ContinuousFuzzySet(domain, lambda x: 0.5)
+        rhs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert lhs.s_norm(rhs, lambda x, y: min(x + y, 1)) == ContinuousFuzzySet(domain, lambda x: 0.9)
+
+    def test_complement_with_invalid_complement_function_raises_value_error(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.0)
+
+        with pytest.raises(ValueError):
+            fs.complement(lambda x: "not a membership degree")
+
+    def test_complement_uses_1_minus_mu_by_default(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert fs.complement() == ContinuousFuzzySet(domain, lambda x: 0.6)
+
+    def test_repr_returns_string_representation_of_the_fuzzy_set(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert repr(fs) == "ContinuousFuzzySet(ContinuousDomain(start=1.0, end=2.6, step=0.5))"
+
+    def test_str_returns_string_representation_of_the_fuzzy_set(self):
+        domain = ContinuousDomain(1.0, 2.6, 0.5)
+        fs = ContinuousFuzzySet(domain, lambda x: 0.4)
+
+        assert str(fs) == "1.0/0.40 + 1.5/0.40 + 2.0/0.40 + 2.5/0.40"
